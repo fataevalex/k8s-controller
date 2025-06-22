c7390447fc8a506e1147ce8ce197dcbc7ae6af80
Oleksandr Fataiev <fataevalex@gmail.com>
2025-06-22 20:38:20 +0300

    step2-zerolog-logging: Add zerolog lib to implement logging in project

--- main.go
+++ main.go
@@ -4,8 +4,16 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/fataevalex/k8s-controller/cmd"
+import (
+// "github.com/fataevalex/k8s-controller/cmd"
+"github.com/rs/zerolog/log"
+)
 
 func main() {
-	cmd.Execute()
+ log.Info().Msg("info message")
+    log.Debug().Msg("debug message")
+    log.Trace().Msg("trace message")
+    log.Warn().Msg("warn message")
+    log.Error().Msg("error message")
+// 	cmd.Execute()
 }
